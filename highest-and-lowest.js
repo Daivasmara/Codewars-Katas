1927dc101767f981c7fc5f8e6ef926d923ed24a7
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-08-25 13:41:58 +0700

    highest and lowest

--- highest-and-lowest.js
+++ highest-and-lowest.js
@@ -4,4 +4,4 @@ function highAndLow(numbers) {
   return (`${Math.max.apply(Math, arrInt)} ${Math.min.apply(Math, arrInt)}`);
 }
 
-console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
+console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") === "542 -214");
