a72405977fd5ee2dee74ce5d1e1343ecfcccc783
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-08-25 16:39:58 +0700

    incrementer

---
+++ incrementer.js
@@ -0,0 +1,13 @@
+function incrementer(nums) {
+  return nums.map((num, i) => {
+    let x = num + i + 1;
+    let result = x > 9 ? parseInt(x.toString().split('').pop()) : x
+    return result
+  });
+}
+
+console.log((incrementer([]) === []));
+console.log((incrementer([1, 2, 3]) === [2, 4, 6]));
+console.log((incrementer([4, 6, 7, 1, 3]) === [5, 8, 0, 5, 8]));
+console.log((incrementer([3, 6, 9, 8, 9]) === [4, 8, 2, 2, 4]));
+console.log((incrementer([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 8]) === [2, 4, 6, 8, 0, 2, 4, 6, 8, 9, 0, 1, 2, 2]));
