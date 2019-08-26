b8258d2d26db8c0d519b163b001107705d9bfa71
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-08-26 18:59:28 +0700

    are-they-the-same

--- are-they-the-same.js
+++ are-they-the-same.js
@@ -1,11 +1,13 @@
 function comp(array1, array2) {
   if (!Array.isArray(array1) || !Array.isArray(array2)) return false
   for (let i = 0; i < array2.length; i++) {
-    if (!array1.includes(Math.sqrt(array2[i]))) return false;
+    let sqrt = Math.sqrt(array2[i]);
+    if (!array1.includes(sqrt)) return false;
+    array1.splice(array1.indexOf(sqrt), 1);
   }
   return true;
 }
 
-a1 = [121, 144, 19, 161, 19, 144, 19, 11];
-a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19];
-console.log(comp(a1, a2), true);
+console.log(comp([2, 2, 3], [4, 9, 9]), false);
+console.log(comp([47, 47, 6], [2209, 2209, 36]), true);
+console.log(comp([98, 49, 85, 61, 35, 88], [9604, 2401, 7225, 3721, 1225, 7744]), true);
