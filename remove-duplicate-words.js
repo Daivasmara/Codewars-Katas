19ed10ce0d402f91259093a699790c838f757a73
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-08-27 09:43:37 +0700

    remove-duplicate-words

--- remove-duplicate-words.js
+++ remove-duplicate-words.js
@@ -1,9 +1,5 @@
 function removeDuplicateWords(s) {
-  const arr = s.split(' ');
-  const set = new Set(arr);
-  const result = Array.from(set).join(' ');
-  
-  return result;
+  return [...new Set(s.split(' '))].join(' ');
 }
 
 console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta') === 'alpha beta gamma delta');
