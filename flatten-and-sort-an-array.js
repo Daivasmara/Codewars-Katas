7acdf36265834bedcd4b4136f9d9812303319cda
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-08-27 09:15:56 +0700

    flatten-and-sort-an-array

--- flatten-and-sort-an-array.js
+++ flatten-and-sort-an-array.js
@@ -1,18 +1,11 @@
 "use strict";
 
 function flattenAndSort(array) {
-  let x = [];
-  for(let i = 0; i < array.length; i++){
-    for(let j = 0; j < array[i].length; j++){
-      x.push(array[i][j]);
-      x.sort((a, b) => a - b);
-    }
-  }
-  return x;
+  return [].concat(...array).sort((a, b) => a - b);
 }
 
 
-console.log(flattenAndSort([]) === []);
-console.log(flattenAndSort([[], [1]]) === [1]);
-console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]) === [1, 2, 3, 4, 5, 6, 7, 8, 9]);
-console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]) === [1, 2, 3, 4, 5, 6, 100]);
+console.log(flattenAndSort([]) , []);
+console.log(flattenAndSort([[], [1]]) , [1]);
+console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]) , [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]) , [1, 2, 3, 4, 5, 6, 100]);
