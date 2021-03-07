e29443fe9cf9c65d6e24943dec4595fb54a5ac5e
Muhammad Daivasmara Denaw <daivasmara.denaw@vidio.com>
2021-03-07 17:31:36 +0700

    vowel_count

---
+++ katas/ruby/vowel_count.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+
+class TestVowelCount < Minitest::Test
+  def get_count(str)
+    str.chars.find_all { |char| char =~ /[aiueo]/i}.length
+  end
+
+  def test_vowel_count_empty_string
+    assert_equal(0, get_count(''))
+  end
+
+  def test_vowel_count_word
+    assert_equal(5, get_count('abracadabra'))
+  end
+
+  def test_vowel_count_two_words
+    assert_equal(4, get_count('pear tree'))
+  end
+
+  def test_vowel_count_random
+    assert_equal(13, get_count('o a kak ushakov lil vo kashu kakao'))
+  end
+
+  def test_vowel_count_random_long
+    assert_equal(168, get_count('tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty'))
+  end
+end
